35317fbf0b27c53e0435e3afddfcaabfd10c77a4
Nacelio Barbosa <naceliombdev@gmail.com>
2022-03-09 19:53:07 -0300

    add forms and tables in index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
-import fs, { read, readFile, readFileSync, writeFile, writeFileSync } from "fs";
+import fs, { createReadStream, read, readFile, readFileSync, writeFile, writeFileSync } from "fs";
 import Orator from "./src/models/Orator.js";
 import Music from "./src/models/Music.js";
+import Ata from "./src/models/Ata.js";
 import path from "path";
-import { send } from "process";
 const oratorsFile = "./orators.json";
 const musicsFile = "./musics.json";
+const ataFile = "./ata.json";
 
 const app = express();
 const PORT = 5000;  
@@ -16,7 +17,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
     return res.sendFile(path.join(path.resolve()+"/src/views/index.html"));
 });
-
 
 app.get("/dashboard", (req, res) => {
     return res.sendFile(path.join(path.resolve()+"/src/views/dashboard.html"));
@@ -55,16 +55,13 @@ app.post("/orators", (req,res) => {
             console.log(error);
             return res.status(400).json("Filed read file");
         }
-
         const parsedData = JSON.parse(data);
         const newData = [...parsedData, orator];
-
         writeFile(oratorsFile, JSON.stringify(newData, null, 2), (err) => {
             if(err){
                 console.log("Filed to write updated data to file");
                 return res.status(500).json("Filed to write updated data to file");
             }
-
             console.log("Updated file sucessfully");
         });
     });
@@ -85,11 +82,8 @@ app.put("/orators/:oratorId", (req, res) => {
             console.log(error);
             return res.status(400).json("Filed read file");
         }
-
         let parsedData = JSON.parse(data);
-
         const foundOratorIndex = parsedData.findIndex( (orator) => orator.id == oratorId );
-
         if( foundOratorIndex === -1 ){
             return res.status(404).json({
                 message: 'Orator not found',
@@ -124,18 +118,14 @@ app.put("/orators/:oratorId", (req, res) => {
                 console.log("Filed to write updated data to file");
                 return res.status(500).json("Filed to write updated data to file");
             }
-            
             console.log("Updated file sucessfully");
             return res.status(200).json({
                 message: 'Orator found and updated',
                 success: true,
                 orators: parsedData
             });
-
         });
     });
-
-    
 });
 
 
@@ -145,11 +135,8 @@ app.get("/musics", (req,res) => {
             console.log(error);
             return res.status(400).json("Filed read file");
         }
-
         const parsedData = JSON.parse(data);
-
         return res.status(200).json(parsedData);
-        
     });
 });
 
@@ -157,33 +144,78 @@ app.post("/musics", (req,res) => {
     const { id } = req.body;
     const { name } = req.body;
     const { page } = req.body;
-
     if (!name || !page){
         return res.status(400).json("Invalid input data");
     }
-
     const music = new Music(id, name, page);
-
     readFile(musicsFile, (error, data) => {
         if(error){
             console.log(error);
             return res.status(400).json("Filed read file");
         }
-
         const parsedData = JSON.parse(data);
         const newData = [...parsedData, music];
-
         writeFile(musicsFile, JSON.stringify(newData, null, 2), (err) => {
             if(err){
                 console.log("Filed to write updated data to file");
                 return res.status(500).json("Filed to write updated data to file");
             }
-
             console.log("Updated file sucessfully");
         });
     });
     return res.status(200).json(music);
+});
 
+app.get("/ata", (req, res) =>{
+    readFile(ataFile, (error, data) => {
+        if(error){
+            console.log(error);
+            return res.status(400).json("Filed read file");
+        }
+        const parsedData = JSON.parse(data);
+        return res.status(200).json(parsedData);
+    });
+});
+    
+app.post("/ata", (req, res) => {
+    const { title } = req.body;
+    readFile(ataFile, (error, data) => {
+        if(error){
+            console.log(error);
+            return res.status(400).json("Filed read file");
+        }
+        let parsedData = JSON.parse(data);
+        readFile(oratorsFile, (error, data) => {
+            if(error){
+                console.log(error);
+                return res.status(400).json("Filed read file");
+            }
+            const parsedOrators = JSON.parse(data);
+            const firstPrayIndex = parsedOrators.findIndex( (orator) => orator.firstPray == true );
+            const lastPrayIndex = parsedOrators.findIndex( (orator) => orator.lastPray == true );
+            readFile(musicsFile, (error, data) =>{
+                if(error){
+                    console.log(error);
+                    return res.status(400).json("Filed read file");
+                }
+                const parsedMusics = JSON.parse(data);
+                const newData = new Ata(title, parsedOrators, parsedOrators[firstPrayIndex], parsedOrators[lastPrayIndex], parsedMusics);
+                parsedData = [newData]
+                writeFile(ataFile, JSON.stringify(parsedData, null, 2), (err) => {
+                    if(err){
+                        console.log("Filed to write updated data to file");
+                        return res.status(500).json("Filed to write updated data to file");
+                    }
+                    console.log("Updated file sucessfully");
+                    return res.status(200).json({
+                        message: 'Ata created successfully',
+                        success: true,
+                        ata: parsedData
+                    });
+                });
+            });
+        });
+    });
 });
 
 app.listen(PORT, () => {
---
+++ src/models/Ata.js
@@ -0,0 +1,11 @@
+class Ata{
+    constructor(title, orators=[], firstPray, lastPray, musics=[]){
+        this.title = title;
+        this.orators = orators;
+        this.firstPray = firstPray;
+        this.lastPray = lastPray;
+        this.musics = musics;
+    }
+}
+
+export default Ata;
